46b4fcf568c80bdb9a022b58d8611e2818f587e5
lapinRagnar <lapinragnar@gmail.com>
2023-11-19 19:58:49 +0100

    The threeJS camera

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Customizer from "./pages/Customizer"
-import Canvas from "./canvas"
+import CanvasModel from "./canvas"
 import Home from "./pages/Home"
 
 
@@ -9,7 +9,7 @@ function App() {
   return (
     <main className="app transition-all ease-in">
       <Home />
-      <Canvas />
+      <CanvasModel />
       <Customizer />
     </main>
   )
--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,9 +1,27 @@
+import { Canvas } from "@react-three/fiber"
+import { Environment, Center } from "@react-three/drei"
 
+import Shirt from "./Shirt"
+import Backdrop from "./Backdrop"
+import CameraRig from "./CameraRig"
+import { AmbientLight } from "three"
 
-const Canvas = () => {
+const CanvasModel = () => {
   return (
-    <div>canvas</div>
+    <Canvas>
+
+      <ambientLight intensity={0.5}/>
+      <Environment preset="city"/>
+
+      <CameraRig>
+        <Center>
+          <Shirt />
+        </Center>
+      
+      </CameraRig>
+
+    </Canvas>
     )
 }
 
-export default Canvas
+export default CanvasModel
